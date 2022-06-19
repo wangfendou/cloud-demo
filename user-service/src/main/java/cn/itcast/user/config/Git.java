79510020b6c2bce344eb6a0904c5ba45a2dc5847
eric <1123943309@qq.com>
2022-06-19 16:41:03 +0800

    git类添加一行

--- user-service/src/main/java/cn/itcast/user/config/Git.java
+++ user-service/src/main/java/cn/itcast/user/config/Git.java
@@ -3,5 +3,6 @@ package cn.itcast.user.config;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
