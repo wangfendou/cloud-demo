eadac1f18878e609ea477253d65f5a4f539d9ff7
eric <1123943309@qq.com>
2022-06-19 22:15:36 +0800

    push test

--- user-service/src/main/java/cn/itcast/user/config/Git.java
+++ user-service/src/main/java/cn/itcast/user/config/Git.java
@@ -6,5 +6,6 @@ public class Git {
         System.out.println("hello git1");
         System.out.println("master commit");
         System.out.println("fix commit");
+        System.out.println("push test");
     }
 }
